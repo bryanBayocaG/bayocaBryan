79c03065ff0f43406ccfe93307ba50c3c112ee03
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-05-23 20:06:41 +0800

    hero section me

--- src/components/sections/HeroSection/HeroSection.tsx
+++ src/components/sections/HeroSection/HeroSection.tsx
@@ -2,16 +2,21 @@
 
 function HeroSection() {
     return (
-
         <section
             id="home"
             className="hero-path relative h-[95vh] bg-[url(/polygonialBGW.svg)] dark:bg-[url(/polygonialBG.svg)] bg-center bg-size-[300%] md:bg-size-[200%] bg-no-repeat bg-[#f7f7f7] dark:bg-[#0f172a]">
-            <div className="absolute top-[30%] flex flex-col md:flex-row items-center justify-between w-full p-[4rem]">
-                <div>
-                    <h1>Hi there! I'm <span className="">Bryan!</span></h1>
+            <div className="absolute flex  flex-col md:flex-row items-center justify-between w-full h-full ">
+                <div className="w-full h-full  relative">
+                    <div className="absolute right-0 bottom-[4.5rem] md:bottom-none md:top-[15rem] h-[50rem] lg:top-2 lg:h-[55rem]">
+                        <img src="/bryanLeft.png" className=" w-full h-full" alt="bryan photo" />
+                    </div>
+                    <p>Part coder</p>
                 </div>
-                <div className="hidden md:block">
-                    ppic ko
+                <div className="hidden md:block w-full h-full  relative">
+                    <div className="absolute left-0 top-[15rem]  h-[50rem]  lg:top-2 lg:h-[55rem]">
+                        <img src="/bryanRight.png" className=" w-full h-full" alt="bryan photo" />
+                    </div>
+                    <p>Part coder</p>
                 </div>
             </div>
 

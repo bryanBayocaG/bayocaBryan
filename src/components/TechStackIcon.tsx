940b771ba373f8228c0db813ebebc22fb5f5399c
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-05-24 16:11:35 +0800

    hero section image optimizaTION

--- src/components/TechStackIcon.tsx
+++ src/components/TechStackIcon.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 function TechStackIcon({ image, name }: Props) {
     return (
-        <div className="bg-white/30 p-[.5rem] w-[7rem] rounded-[.3rem] flex flex-col gap-[.5rem] justify-center items-center shadow-md hover:shadow-lg hover:scale-110">
+        <div className="bg-white/30 p-[.5rem] w-[7rem] rounded-[.3rem] flex flex-col gap-[.5rem] justify-center items-center shadow-md hover:shadow-lg hover:scale-110 transition-all duration-300">
             <div className="w-3rem h-[3rem]">
                 <img className="w-full h-full " src={image} alt="tech_logo" />
             </div>
--- src/components/sections/HeroSection/HeroSection.tsx
+++ src/components/sections/HeroSection/HeroSection.tsx
@@ -4,18 +4,16 @@ import { useEffect, useRef } from 'react';
 function HeroSection() {
     const nonColorMeRef = useRef<HTMLDivElement | null>(null);
     const coloredMeRef = useRef<HTMLDivElement | null>(null);
+    const noColoredMeBG = useRef<HTMLDivElement | null>(null);
     useEffect(() => {
         const ctx = gsap.context(() => {
-            gsap.fromTo(nonColorMeRef.current, { y: 300, opacity: 0 }, {
+            gsap.fromTo([nonColorMeRef.current, noColoredMeBG.current], { y: 300 }, {
                 y: 0,
-                opacity: 1,
                 duration: 2,
                 ease: "power2.out",
             });
-            gsap.fromTo(coloredMeRef.current, { y: -100, opacity: 0 }, {
+            gsap.fromTo(coloredMeRef.current, { y: -100 }, {
                 y: 0,
-
-                opacity: 1,
                 duration: 2,
                 ease: "power2.out",
             });
@@ -30,32 +28,29 @@ function HeroSection() {
             <div className="absolute flex  flex-col md:flex-row items-center justify-between w-full h-full ">
                 <div className="w-full h-full  relative">
                     <div ref={nonColorMeRef} className="absolute right-0 bottom-[4.5rem] md:bottom-none md:top-[15rem] h-[50rem] lg:top-2 lg:h-[55rem] ">
-                        <img src="/bryanLeftWEBP.webp" className="pointer-events-none w-full h-full" alt="bryan photo" draggable={false} />
-                        {/* <img src="/bryanLeft.png" className="pointer-events-none w-full h-full" alt="bryan photo" draggable={false} /> */}
+                        <img src="/bryanLeft.webp" className="pointer-events-none w-full h-full" alt="bryan photo" draggable={false} />
+                    </div>
+                    <div ref={noColoredMeBG} className="absolute right-[3.5rem] bottom-[15rem] w-[45rem] rotate-[-35deg] md:right-[5.5rem] md:w-[35rem] md:bottom-[12.5rem] lg:bottom-[15rem] lg:right-[2.5rem] lg:w-[45rem] -z-10">
+                        <img src="/jsPHPtext.webp" className="pointer-events-none" alt="bgText" draggable={false} />
                     </div>
                     <div className='mt-[30%] flex flex-col justify-center items-center w-fit mx-auto md:mx-0 md:mr-auto relative p-[1rem]'>
-                        <div className='absolute left-[5rem] top-7'>
+                        <div className='absolute left-[6rem] top-7'>
                             <p className='text-[1.5rem] font-bold'>I do</p>
                         </div>
                         <p className='text-[5rem] font-extrabold'>&lt;coding&gt;</p>
-                    </div>
-                    <div className='-mt-[2rem] w-[20rem] flex flex-col justify-center items-center mx-auto md:mx-0 md:mr-auto relative p-[1rem]'>
-                        <p className='text-justify'>I am a full-stack developer, I'm proficient in PHP and JavaScript, building full-stack applications that are both functional and visually polished.</p>
+                        <p className='w-[20rem] text-justify'>I am a full-stack developer, I'm proficient in PHP and JavaScript, building full-stack applications that are both functional and visually polished.</p>
                     </div>
                 </div>
                 <div className="hidden md:block w-full h-full  relative">
                     <div ref={coloredMeRef} className="absolute left-0 top-[15rem]  h-[50rem]  lg:top-2 lg:h-[55rem] ">
-                        <img src="/bryanRightWEBP.webp" className="pointer-events-none w-full h-full" alt="bryan photo" draggable={false} />
-                        {/* <img src="/bryanRight.png" className="pointer-events-none w-full h-full" alt="bryan photo" draggable={false} /> */}
+                        <img src="/bryanRight.webp" className=" pointer-events-none w-full h-full" alt="bryan photo" draggable={false} />
                     </div>
-                    <div className=' mt-[30%] flex flex-col justify-end items-center w-fit ml-auto relative p-[1rem]'>
-                        <div className='absolute left-[5rem] top-7'>
+                    <div className='mt-[30%] flex flex-col justify-end items-center w-fit ml-auto relative p-[1rem] bg-center bg-cover'>
+                        <div className='absolute left-[8.5rem] top-7'>
                             <p className='text-[1.5rem] font-bold'>I do</p>
                         </div>
-                        <p className='text-[5rem] font-bold'>design</p>
-                    </div>
-                    <div className='flex -mt-[2rem] w-[20rem] flex-col justify-end items-center ml-auto relative p-[1rem] bg-white/30'>
-                        <p className='text-justify'>I create my own image assets and logos from scratch. I'm highly proficient with Photoshop and Illustrator, bringing concepts to life with creativity.</p>
+                        <p className='text-[5rem] font-bold '>~design~</p>
+                        <p className='w-[20rem] text-justify p-[.5rem]'>I create my own image assets and logos from scratch. I'm highly proficient with Photoshop and Illustrator, bringing concepts to life with creativity.</p>
                     </div>
                 </div>
             </div>

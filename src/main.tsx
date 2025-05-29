56a2966aad352253efb2c8cce2a6b0a897b4c411
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-05-29 18:16:14 +0800

    update added analytics2

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from "next-themes";
 import './index.css'
 import App from './App.tsx'
-
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider attribute="class" defaultTheme="system">
       <App />
-
+      <Analytics mode="production" />
     </ThemeProvider>
   </StrictMode>,
 )
